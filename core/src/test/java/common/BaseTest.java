ff612f4fc076cc12ad1744ad760eea8d748a7bb2
caixiaowei <caixiaowei@52tt.com>
2021-11-29 15:39:34 +0800

    云掉注释

--- core/src/test/java/common/BaseTest.java
+++ core/src/test/java/common/BaseTest.java
@@ -5,9 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * @author : linguozhi@52tt.com
  * @desc : 测试基类
- * @date :  2018/2/6
  */
 @RunWith(SpringJUnit4ClassRunner.class) //使用junit4进行测试
 @ContextConfiguration(locations={"classpath:rootContext.xml"}) //加载配置文件
